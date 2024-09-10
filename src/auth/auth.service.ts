9abf98c40caacf509f7e27c3c9be1eb0882d8a5e
Đậu Đức Quân <bruceparkerblue@gmail.com>
2024-09-10 23:59:03 +0700

    Migration to docker
    * Downgrade postgres & prisma version
    * Create prisma module
    * Create migration

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,11 +1,11 @@
-import { Injectable } from "@nestjs/common";
-
+import { Injectable } from '@nestjs/common';
+import { User, Bookmark } from '@prisma/client';
 @Injectable({})
-export class AuthService  {
-    signup() {
-        return { message: 'I am signup' };
-    }
-    signin() {
-        return { message: 'I am signin' };
-    }
+export class AuthService {
+  signup() {
+    return { message: 'I am signup' };
+  }
+  signin() {
+    return { message: 'I am signin' };
+  }
 }
