80cbe819ebbf230bf80d2949bc039de83a4aabed
Đậu Đức Quân <bruceparkerblue@gmail.com>
2024-09-14 23:25:37 +0700

    Using passport.js and jwt module

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,6 +4,7 @@ import { AuthModule } from './auth/auth.module';
 import { BookmarkModule } from './bookmark/bookmark.module';
 import { PrismaModule } from './prisma/prisma.module';
 import { UserModule } from './user/user.module';
+import { JwtModule } from '@nestjs/jwt';
 
 @Module({
   imports: [
