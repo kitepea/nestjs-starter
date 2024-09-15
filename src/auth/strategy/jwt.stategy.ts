fd0e0a24bd32ea14b768075262349f351352221e
Đậu Đức Quân <bruceparkerblue@gmail.com>
2024-09-15 08:32:56 +0700

    Implement JwtStrategy and UseGuards method

--- src/auth/strategy/jwt.stategy.ts
+++ src/auth/strategy/jwt.stategy.ts
@@ -2,13 +2,30 @@ import { Injectable } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
 import { PassportStrategy } from '@nestjs/passport';
 import { ExtractJwt, Strategy } from 'passport-jwt';
+import { PrismaService } from 'src/prisma/prisma.service';
 
 @Injectable()
 export class JwtStrategy extends PassportStrategy(Strategy) {
-  constructor(config: ConfigService) {
+  // Can add 'jwt' here
+  constructor(
+    config: ConfigService,
+    private prisma: PrismaService, // This turn prisma from param to property
+  ) {
     super({
       jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
       secretOrKey: config.get('JWT_SECRET'),
     });
+  }
+
+  async validate(payload: { sub: number; email: string }) {
+    // console.log(payload);
+    const user = await this.prisma.user.findUnique({
+      where: {
+        id: payload.sub,
+      },
+    });
+
+    delete user.hash;
+    return user;
   }
 }
