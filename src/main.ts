df4dc2905c9242eef434a733c84a30871bd9a9cf
Đậu Đức Quân <bruceparkerblue@gmail.com>
2024-09-16 23:30:52 +0700

    Change .env.local to .env

--- src/main.ts
+++ src/main.ts
@@ -4,11 +4,6 @@ import * as dotenv from 'dotenv';
 import { existsSync } from 'fs';
 import { AppModule } from './app.module';
 
-if (existsSync('.env.local')) {
-  dotenv.config({ path: '.env.local' });
-} else {
-  dotenv.config();
-}
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(
