7313eedefe05a8a4e8fa61802bdfb4a80200571f
Đậu Đức Quân <bruceparkerblue@gmail.com>
2024-09-18 10:06:49 +0700

    Add SignUp test

--- test/app.e2e-spec.ts
+++ test/app.e2e-spec.ts
@@ -1,6 +1,8 @@
 import { INestApplication, ValidationPipe } from '@nestjs/common';
 import { Test } from '@nestjs/testing';
+import * as pactum from 'pactum';
 import { AppModule } from 'src/app.module';
+import { AuthDto } from '../src/auth/dto';
 import { PrismaService } from '../src/prisma/prisma.service';
 
 describe('App e2e', () => {
@@ -18,6 +20,7 @@ describe('App e2e', () => {
       }),
     );
     await app.init();
+    await app.listen(3333);
 
     prisma = app.get(PrismaService);
     await prisma.cleanDb();
@@ -30,7 +33,18 @@ describe('App e2e', () => {
   // describe: define a group of related tests
   describe('Auth', () => {
     describe('Signup', () => {
-      it.todo('should create a new user');
+      it('should create a new user', () => {
+        const dto: AuthDto = {
+          email: 'quan@gmail.com',
+          password: '1234',
+        };
+        return pactum
+          .spec()
+          .post('http://localhost:3333/auth/signup')
+          .withBody(dto)
+          .expectStatus(201)
+          .inspect();
+      });
     });
     describe('Signin', () => {});
   });
